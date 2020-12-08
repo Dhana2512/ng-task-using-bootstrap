90feee6ee2e5ff72d6ae5fa10e31ecd447572fac
dhana <dhanavidhu256@gmail.com>
2020-12-08 20:05:32 +0530

    Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,35 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {RouterModule, Routes} from '@angular/router';
+import { SignupComponent } from './signup/signup.component';
+import { SignupPasswordComponent } from './signup-password/signup-password.component';
+
+const routes : Routes =[
+  {
+    path:'signup',
+    component :SignupComponent
+  },
+  {
+    path:'signuppassword',
+    component :SignupPasswordComponent
+  },
+  {
+    path:'**',
+    pathMatch : 'full',
+    redirectTo : '/signup'
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    SignupPasswordComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/signup-password/signup-password.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-signup-password',
+  templateUrl: './signup-password.component.html',
+  styleUrls: ['./signup-password.component.css']
+})
+export class SignupPasswordComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
